70b9295fc96e84d19b12a38a3037bc4a2b5ec2ad
abuabdirohman4 <kakaabu354@yahoo.com>
2020-01-07 05:33:12 +0700

    initial commit

---
+++ pertemuan4/function.php
@@ -0,0 +1,23 @@
+<?php
+function salam($waktu, $nama)
+{
+    return "Selamat $waktu, $nama";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Latihan Function</title>
+</head>
+
+<body>
+    <h1><?= salam("Pagi", "Abu"); ?></h1>
+</body>
+
+</html>
---
+++ pertemuan5/latihan1.php
@@ -0,0 +1,14 @@
+<?php
+// Array
+// variabel yang dapat memiliki banyak nilai
+// elemen pada array boleh memiliki tipe data yang berbeda
+
+// membuat array
+// cara lama
+$hari = array("Senin", "Selasa, Rabu");
+// cara baru
+$bulan =["Januari", "Februari", "Maret"];
+$arr = [123, "tulisan", false];
+
+// Menampilkan Array
+// var_dump() /print_f()
