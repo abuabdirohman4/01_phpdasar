705215040dd9b92d39528a78e60680be82c16048
abuabdirohman4 <kakaabu354@yahoo.com>
2021-03-15 16:39:29 +0700

    update

--- pertemuan5/latihan1.php
+++ pertemuan5/latihan1.php
@@ -2,13 +2,33 @@
 // Array
 // variabel yang dapat memiliki banyak nilai
 // elemen pada array boleh memiliki tipe data yang berbeda
+// pasangan antara key dan value
+// key-nya adalah index, yang dimulai dari 0
 
 // membuat array
 // cara lama
 $hari = array("Senin", "Selasa, Rabu");
 // cara baru
-$bulan =["Januari", "Februari", "Maret"];
+$bulan = ["Januari", "Februari", "Maret"];
 $arr = [123, "tulisan", false];
 
 // Menampilkan Array
 // var_dump() /print_f()
+// var_dump($hari);
+// echo "<br>";
+// print_r($bulan);
+// echo "<br>";
+
+// Menampilkan 1 elemen pada array
+// echo $arr[0];
+// echo "<br>";
+// echo $bulan[1];
+
+// Menambahkan elemen baru pada array
+var_dump($hari);
+$hari[] = "Kamis";
+echo "<br>";
+var_dump($hari);
+$hari[] = "Jumat";
+echo "<br>";
+var_dump($hari);
---
+++ pertemuan5/latihan2.php
@@ -0,0 +1,33 @@
+<?php
+// Pengulangan pada array
+// for / foreach
+$angka = [3, 2, 15, 20, 11, 77, 89]
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Latihan 2</title>
+    <style>
+        div {
+            width: 50px;
+            height: 50px;
+            background-color: salmon;
+            text-align: center;
+            line-height: 50px;
+            margin: 3px;
+            float: left;
+        }
+    </style>
+</head>
+
+<body>
+    <div>3</div>
+</body>
+
+</html>
