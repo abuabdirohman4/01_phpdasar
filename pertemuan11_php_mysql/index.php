189ab590c46a406f3888946ba481f65d3051a7c8
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-06-13 23:34:17 +0700

    Eps 10 : Database & MySQL

---
+++ pertemuan11_php_mysql/index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Halaman Admin</title>
+    </head>
+    <body>
+        <h1>Daftar Mahasiswa</h1>
+
+        <table border="1" cellpadding="10" cellspacing="0">
+            <thead>
+                <td>No</td>
+                <td>Aksi</td>
+                <td>Gambar</td>
+                <td>NRP</td>
+                <td>Nama</td>
+                <td>Email</td>
+                <td>Jurusan</td>
+            </thead>
+            <tbody>
+                <td></td>
+            </tbody>
+        </table>
+    </body>
+</html>
